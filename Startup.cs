3d42954eed97bfa05b7c06d088b5edb0c0a69647
SetMa02 <maxuser99@mail.ru>
2021-06-08 20:36:08 +0700

    gitPractice

--- Startup.cs
+++ Startup.cs
@@ -12,13 +12,14 @@ namespace ASPNETCorePractice
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews().SetCompatibilityVersion
+                (Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
+
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -27,6 +28,7 @@ namespace ASPNETCorePractice
             }
 
             app.UseRouting();
+            app.UseStaticFiles();
 
             app.UseEndpoints(endpoints =>
             {
